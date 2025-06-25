55d8addefcd308b78fabd18d7f9da9fcfcce084d
Beatrice <beatrice.viscogliosi@umontreal.ca>
2025-06-25 17:47:03 -0400

    Added mst_dataloading provided by BF and Cryocore manual controls

---
+++ samples/test/Cryocore/manual controls.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Jun 14 15:54:30 2025
+
+@author: dtiede
+"""
+
+import requests 
+
+
+requests.post('http://192.168.1.108:47101/v1/controller/methods/warmup()')
