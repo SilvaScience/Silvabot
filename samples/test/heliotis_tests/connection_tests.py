f962db40de1b73246b8410d62fcaeb92fcf19e96
Beatrice <beatrice.viscogliosi@umontreal.ca>
2025-06-25 19:48:04 -0400

    Created draft of 1D scan function (only stage moving + phase acquisition for now)

--- samples/test/heliotis_tests/connection_tests.py
+++ samples/test/heliotis_tests/connection_tests.py
@@ -6,7 +6,7 @@ Created on Tue May 27 15:58:25 2025
 """
 
 import sys
-print(sys.prefix)
+print('sys.prefix, sys.executable: ', sys.prefix, sys.executable)
 
 from jki_python_bridge_for_labview import labview as lv
 
@@ -16,6 +16,7 @@ unit_map = {
     'meV': 1,
     'THz': 2,
     'cm-1': 3
+
 }
 
 def units(string):
@@ -50,13 +51,21 @@ print('phase_AB,phase_CD: ', phase_AB, phase_CD)
 
 
 
-#Loop to discretely scan 0-1ps with 10 points
-for i in range(11):
-    print(i)
-    lv.LV_Control.move_stage_pos(2, (i/10))
-    print(lv.LV_Control.check_stage_move())
-    phase_AB, phase_CD = lv.LV_Control.acquire_phase()
-    print('phase_AB,phase_CD: ', phase_AB, phase_CD)
+#Loop for 1D discrete scan (starts at t=0ps)
+def OneD_scan(resolution, scan_length):  #resolution: points/picosecond, scan_length: length of t-axis in picosecond
+    phase_AB_table = []
+    phase_CD_table = []
+    for i in range(resolution*scan_length): #can add a +1 if want to include the last extremity of the time interval
+        print(i)
+        lv.LV_Control.move_stage_pos(2,i/resolution)
+        #print(lv.LV_Control.check_stage_move())
+        phase_AB, phase_CD = lv.LV_Control.acquire_phase()
+        phase_AB_table.append(phase_AB)
+        phase_CD_table.append(phase_CD)
+    print('phase_AB_table,phase_CD_table: ', phase_AB_table, phase_CD_table)
+
+    #ONLY PHASE ACQUISITION IS THERE FOR THE MOMENT (TO KNOW STAGES' POSITIONS)
+    #WILL ADD THE REST OF THE SCAN INSTRUCTIONS TOMORROW (get detected spectrum for each "i" and FT S(t1) of each nu along "i-axis" to get back emission for every nu)   nu=detection frequency
 
 
-
+OneD_scan(10,2)
