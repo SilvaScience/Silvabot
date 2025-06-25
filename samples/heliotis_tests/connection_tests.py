cf6a3f8286f395201a286e6434ee94ea75772184
Beatrice <beatrice.viscogliosi@umontreal.ca>
2025-06-25 16:59:49 -0400

    Added discrete t-axis + phase measurements in connection_tests.py

--- samples/heliotis_tests/connection_tests.py
+++ samples/heliotis_tests/connection_tests.py
@@ -24,8 +24,39 @@ def units(string):
     except KeyError:
         raise ValueError(f"Unknown unit: {string}")
         
+import os
+print(os.getuid() if hasattr(os, 'getuid') else os.system("whoami"))     #added to see if running as admin
+
+
 
 
 lv.connect()
 serv_connect = lv.isConnected
-print(serv_connect)
+print('serv_connect: ', serv_connect)
+
+
+
+
+#move_stage_pos(stage_sel,position in ps)
+#stage_sel: 0(tau), 1 (T), 2 (t)
+lv.LV_Control.move_stage_pos(2,8)
+
+
+print(lv.LV_Control.check_stage_move())
+
+
+phase_AB, phase_CD = lv.LV_Control.acquire_phase()
+print('phase_AB,phase_CD: ', phase_AB, phase_CD)
+
+
+
+#Loop to discretely scan 0-1ps with 10 points
+for i in range(11):
+    print(i)
+    lv.LV_Control.move_stage_pos(2, (i/10))
+    print(lv.LV_Control.check_stage_move())
+    phase_AB, phase_CD = lv.LV_Control.acquire_phase()
+    print('phase_AB,phase_CD: ', phase_AB, phase_CD)
+
+
+
