4891a263af1d954757a0fd50fe400b6fb713d658
Étienne Tremblay <etremblay597@gmail.com>
2026-01-16 09:32:48 -0500

    Modified the UHF.py code to take two signal inputs in PlotterWorker
    The UHF.py code has been modified so that the PlotterWorker class can now take two signal inputs. This allows to demodulate the two signal inputs individually and to then substract then to get the balanced signal. The substraction of the signals and the plotting still needs to be implemented.

--- src/drivers/UHF.py
+++ src/drivers/UHF.py
@@ -15,16 +15,11 @@ class UHF():
 
         # setting up the parameter dict
         self.parameter_dict = defaultdict()
-
-###################### These parameters need to be changed
-        self.total_duration = 5
-        self.sampling_rate = 1000
-        self.burst_duration = 0.2        
-        self.parameter_display_dict = defaultdict(dict)
-
         self.parameter_dict['sampling_rate'] = 0
         self.parameter_dict['filter_order'] = 0
         self.parameter_dict['time_constant'] = 0
+        self.parameter_dict['Displayed_signal_input'] = 0
+        self.parameter_display_dict = defaultdict(dict)
 
 
         self.parameter_display_dict['sampling_rate']['val'] = 1000
@@ -40,6 +35,10 @@ class UHF():
         self.parameter_display_dict['time_constant']['unit'] = 's'
         self.parameter_display_dict['time_constant']['max'] = 100
         self.parameter_display_dict['time_constant']['read'] = False
+        self.parameter_display_dict['Displayed_signal_input']['val'] = 1
+        self.parameter_display_dict['Displayed_signal_input']['unit'] = ' '
+        self.parameter_display_dict['Displayed_signal_input']['max'] = 2
+        self.parameter_display_dict['Displayed_signal_input']['read'] = False
 
         # set up parameter dict that only contains value
         self.parameter_dict = {}
@@ -51,12 +50,18 @@ class UHF():
         self.device = self.session.connect_device("DEV2037")    # Connect to the UHF device (ID DEV2037)
         print('Connection established with the Lock-In')    
 
-        # Configure the signal input
+        # Configure the signal input 1
         self.device.sigins[0].range(1.5)  # Set input range to 1.5 V
         self.device.sigins[0].ac(False)   # Set the device to DC coupling
         self.device.sigins[0].imp50(True) # Set the input impedance to 50 Ohm
 
-        # Configure the demodulation
+        # Configure the signal input 2
+        self.device.sigins[1].range(1.5)  # Set input range to 1.5 V
+        self.device.sigins[1].ac(False)   # Set the device to DC coupling
+        self.device.sigins[1].imp50(True) # Set the input impedance to 50 Ohm
+
+        # Configure the first demodulation
+        self.device.demods[0].adcselect(0)                                         # Select the input channel to use
         self.device.demods[0].rate(self.parameter_dict['sampling_rate'])           # Set the sampling rate
         self.device.demods[0].enable(True)                                         # Enable the demodulator
         self.device.demods[0].order(self.parameter_dict['filter_order'])           # Set the filter order
@@ -66,6 +71,16 @@ class UHF():
         self.device.extrefs[0].enable(1)                                           # Enable external reference
         self.device.demods[3].adcselect(2)                                         # Select the input channel to use for the reference
 
+        # Configure the second demodulation
+        self.device.demods[4].adcselect(1)                                         # Select the input channel to use
+        self.device.demods[4].rate(self.parameter_dict['sampling_rate'])           # Set the sampling rate
+        self.device.demods[4].enable(True)                                         # Enable the demodulator
+        self.device.demods[4].order(self.parameter_dict['filter_order'])           # Set the filter order
+        self.device.demods[4].timeconstant(self.parameter_dict['time_constant'])   # Set the time constant
+        self.device.demods[4].oscselect(1)                                         # Set the oscillator to use
+        self.device.demods[4].sinc(1)                                              # Enable sinc filter
+        self.device.extrefs[1].enable(1)                                           # Enable external reference
+        self.device.demods[7].adcselect(2)                                         # Select the input channel to use for the reference
 
         # Configure the scope parameters
         self.scope_module = self.session.modules.scope # Create scope module
@@ -73,6 +88,7 @@ class UHF():
         self.wave_node = self.device.scopes[0].wave    # Define node to acquire data from
         self.scope_module.subscribe(self.wave_node)    # Subscribe to the scope wave node
         with self.device.set_transaction():
+            self.device.scopes[0].channel(self.parameter_dict['Displayed_signal_input']) # Select the input channel to acquire
             self.device.scopes[0].trigenable(True)   # Enable the scope trigger
             self.device.scopes[0].trigchannel(3)     # Selection which input to use for the triger (0 : sig in 1, 1 : sig in 2, 1: ref trigger 1, 2: ref trigger 2)
             self.device.scopes[0].trigrising(1)      # Trigger on rising edge
@@ -93,6 +109,9 @@ class UHF():
         if parameter == 'time_constant':
             self.update_time_constant(value)
             self.time_constant = value
+        if parameter == 'Displayed_signal_input':
+            self.update_displayed_signal_input(value)
+            self.parameter_dict['Displayed_signal_input'] = value
 
     def update_sampling_rate(self, sampling_rate):
         self.device.demods[0].rate(sampling_rate)
@@ -106,31 +125,33 @@ class UHF():
         self.device.demods[0].timeconstant(time_constant)
         print(f'Time constant set to {time_constant} s')
 
+    def update_displayed_signal_input(self, displayed_signal_input):
+        self.device.scopes[0].channel(displayed_signal_input)
+        print(f'Displayed signal input set to channel {displayed_signal_input}')
+
     def Scope_acquire(self):
-        while True:
-            self.scope_module.execute()         # Start the scope acquisition
-            self.device.scopes[0].enable(True)  # Enable the scope
-            self.session.sync()                 # Sync the session
+        self.device.scopes[0].channels[0].inputselect(self.parameter_dict['Displayed_signal_input'])
+        self.scope_module.execute()         # Start the scope acquisition
+        self.device.scopes[0].enable(True)  # Enable the scope
+        self.session.sync()                 # Sync the session
 
-            # Wait until at least one record is available
-            while self.scope_module.records() == 0:
-                time.sleep(0.05) 
+        # Wait until at least one record is available
+        while self.scope_module.records() == 0:
+            time.sleep(0.05) 
 
-            data = self.scope_module.read()       # Read the scope data
-            self.device.scopes[0].enable(False)   # Disable the scope
-            self.scope_module.finish()            # Finish the scope acquisition
-            self.records = data[self.wave_node]   # Get the records from the wave node
-            self.record = self.records[-1][0]                    # Get the most recent record
-            self.wave = self.record["wave"][0, :]                # Extract the waveform data
-            self.totalsamples = self.record["totalsamples"]      # Extract the total number of samples
-            self.dt = self.record["dt"]                          # Extract the time step
-            self.timestamp = self.record["timestamp"]            # Extract the timestamp
-            self.triggertimestamp = self.record["triggertimestamp"]  # Extract the trigger timestamp
-            t_us = 1e6 * np.arange(-self.totalsamples, 0) * self.dt + (self.timestamp - self.triggertimestamp) / float(self.clockbase) # Create time array            
-            return t_us, self.wave
+        data = self.scope_module.read()       # Read the scope data
+        self.device.scopes[0].enable(False)   # Disable the scope
+        self.scope_module.finish()            # Finish the scope acquisition
+        self.records = data[self.wave_node]   # Get the records from the wave node
+        self.record = self.records[-1][0]                    # Get the most recent record
+        self.wave = self.record["wave"][0, :]                # Extract the waveform data
+        self.totalsamples = self.record["totalsamples"]      # Extract the total number of samples
+        self.dt = self.record["dt"]                          # Extract the time step
+        self.timestamp = self.record["timestamp"]            # Extract the timestamp
+        self.triggertimestamp = self.record["triggertimestamp"]  # Extract the trigger timestamp
+        t_us = 1e6 * np.arange(-self.totalsamples, 0) * self.dt + (self.timestamp - self.triggertimestamp) / float(self.clockbase) # Create the time array
+        return t_us, self.wave
 
-    def FFT_acquire(self, terminate):
-        print(0)
 
 class PlotterWorker(QtCore.QThread):
     scan_data = QtCore.pyqtSignal(np.ndarray, np.ndarray)
@@ -138,39 +159,61 @@ class PlotterWorker(QtCore.QThread):
     def __init__(self, device):
         super().__init__()
         self.is_running = False
-        self.device = device.device  # Extract the actual device from the UHF instance
-        self.session = device.session
-        self.clockbase = device.clockbase
+        self.device = device.device        # Access the device from the UHF class
+        self.session = device.session      # Access the session from the UHF class
+        self.clockbase = device.clockbase  # Access the clockbase from the UHF class
 
     def run(self):
         self.is_running = True
-        sample_node = self.device.demods[0].sample
-        sample_node.subscribe()
-         
-        # Storage for 'continuous' data
-        time_array = []
-        R_array = []
         
-        start_time = time.time()
-        last_time = start_time
-        
-        # Start polling loop
-        while self.is_running:
-            data = self.session.poll(0.1)   # Poll for new data from the Data Server
-            if sample_node in data:
-                samples = data[sample_node]                    # Get samples from the node
-                R = np.sqrt(samples["x"]**2 + samples["y"]**2) # Compute amplitude R from x and y
-                ts = (samples["timestamp"] - samples["timestamp"][0]) / self.clockbase # Convert timestamps to seconds
-                
-                # Append the data to the storage arrays
-                R_array.extend(R)
-                time_array.extend(ts + (last_time - start_time))
-                last_time = time.time()
+        # Function to read data from the DAQ module and to store it in results dictionary
+        def read_data(daq_module, results, ts0):
+            daq_data = daq_module.read(raw=False, clk_rate=self.clockbase)
+            for node in sample_nodes:
+                if node in daq_data.keys():
+                    for sig_burst in daq_data[node]:
+                        results[node].append(sig_burst)
+                        if np.any(np.isnan(ts0)):
+                            ts0 = sig_burst.header['createdtimestamp'][0] / self.clockbase
+            return results, ts0
 
-            time.sleep(0.1) # buffer
-        
-        sample_node.unsubscribe() # Unsubscribe from the node when done
-        self.scan_data.emit(np.array(time_array), np.array(R_array)) # Emit the collected scan data
+        # Initialize DAQ module for burst acquisition
+        sample_nodes = [self.device.demods[0].sample.r,   # Select Demod 0's R output
+                        self.device.demods[4].sample.r]   # Select Demod 4's R output
+        burst_duration = 0.2                              # Duration of each data burst in seconds
+        num_cols = int(np.ceil(burst_duration * self.device.demods[0].rate()))  # Number of samples per burst
+        daq_module = self.session.modules.daq   # Create DAQ module
+        daq_module.device(self.device)          # Link the DAQ module to the UHF device
+        daq_module.type(0)                      # Set DAQ type to 'burst'
+        daq_module.grid.mode(2)                 # Set grid mode
+        daq_module.endless(1)                   # Enable endless acquisition
+        daq_module.duration(burst_duration)     # Set burst duration
+        daq_module.grid.cols(num_cols)          # Set number of columns in the grid based on burst duration and sampling rate
+        for node in sample_nodes:
+            daq_module.subscribe(node)          # Subscribe to the sample nodes
+
+        # Acquire data in bursts until stopped
+        ts0 = np.nan                             # Initialize initial timestamp
+        results = {x: [] for x in sample_nodes}  # Initialize results dictionary
+        daq_module.execute()                     # Start the DAQ module
+        while self.is_running:                   # Loop until stopped (stopped when scan is finished)
+            results, ts0 = read_data(daq_module, results, ts0)  # Read data and update results
+            time.sleep(burst_duration)                          # Wait for the duration of the burst before next read
+        daq_module.finish()                                     # Stop the DAQ module
+        results, ts0 = read_data(daq_module, results, ts0)      # Final read to get any remaining data
+
+        # Organize the acquired data
+        d0_bursts = results[sample_nodes[0]]                                                                            # Get bursts for Demod 0
+        r0 = np.concatenate([b.value.flatten() for b in d0_bursts])                                                     # Concatenate R values for Demod 0
+        t0 = np.concatenate([(b.time + (b.header['createdtimestamp'][0] / self.clockbase) - ts0) for b in d0_bursts])   # Concatenate time values for Demod 0
+        d4_bursts = results[sample_nodes[1]]                                                                            # Get bursts for Demod 4
+        r4 = np.concatenate([b.value.flatten() for b in d4_bursts])                                                     # Concatenate R values for Demod 4
+        t4 = np.concatenate([(b.time + (b.header['createdtimestamp'][0] / self.clockbase) - ts0) for b in d4_bursts])   # Concatenate time values for Demod 4
+
+        # Calculate the difference in R values and plot it
+        # R_diff = r0 - r4
+        # plt.plot(t0, R_diff)
+        # plt.show()
 
     def stop(self):
         self.is_running = False
