9fe0add4a782dc3fccecf2321c36ecb8a12dac7f
Isabella Moreno <isabella.moreno@umontreal.ca>
2026-07-22 11:13:42 -0400

    Update Communitation_test.py

---
+++ samples/test/Harpia/Communitation_test.py
@@ -0,0 +1,51 @@
+import time
+import sys
+from lightcon.harpia import Harpia
+
+
+#serialNumber = "M24100"
+#base_adress = "http://localhost:20050/v1/"
+
+ip_address = "192.168.1.134"
+
+#initialize connection to Harpia
+harpia = Harpia(ip_address)
+# check if connection is established
+if not harpia.connected:
+    sys.exit("Could not connect to Harpia")
+
+
+
+# test delay line
+# Read actual delay line position
+current = harpia.delay_line_actual_delay()
+print("Current delay:", current, "ps")
+
+# Move to 1 ps
+target_delay = 1.0
+
+print("Moving to", target_delay, "ps")
+harpia.set_delay_line_target_delay(target_delay)
+
+# Wait for the delay line to reach the target position
+while True:
+    current = harpia.delay_line_actual_delay()
+    print("Current delay:", current, "ps")
+    if abs(current - target_delay) < 0.001:  # tolerance of 1 fs = 0.001 ps
+        break
+    time.sleep(0.2)
+
+print("Delay reached!")
+
+
+
+
+
+
+
+
+
+
+
+
+
